b1663fccdbd987be8a7a88495df22554c42f82aa
eloy <eloy.colell@gmail.com>
2015-02-18 00:39:43 -0300

    It improve the unittests.

--- tests/instrument_test.py
+++ tests/instrument_test.py
@@ -10,6 +10,7 @@ from netcdf import netcdf as nc
 class assert_downloaded(object):
 
     def __init__(self, test_case, should_download):
+        self.files = []
         self.should_download = should_download
         self.assertEquals = test_case.assertEquals
         self.assertNotEquals = test_case.assertNotEquals
@@ -23,7 +24,8 @@ class assert_downloaded(object):
 
     def downloaded(self, file_list):
         self.files = file_list
-        self.initial_files = filter(lambda f: f in self.files, self.initial_files)
+        self.initial_files = filter(lambda f: f not in self.files,
+                                    self.initial_files)
 
     def __exit__(self, type, value, traceback):
         self.initial_size = len(self.initial_files)
@@ -32,31 +34,24 @@ class assert_downloaded(object):
         if files:
             # Test if it downloaded something.
             self.assertNotEquals(final_size, self.initial_size)
-            self.assertEquals(len(files), final_size - self.initial_size)
+            the_size = final_size - self.initial_size
+            self.assertEquals(len(files), the_size)
             # Test if it calibrated the downloaded files.
             with nc.loader(files) as root:
                 counts = nc.getvar(root, 'counts_shift')
-                self.assertEquals(counts.shape, (final_size, 1, 1))
+                self.assertEquals(counts.shape, (the_size, 1, 1))
                 self.assertEquals(counts[0], 32.)
                 space = nc.getvar(root, 'space_measurement')
-                self.assertEquals(space.shape, (final_size, 1, 1))
+                self.assertEquals(space.shape, (the_size, 1, 1))
                 self.assertEquals(space[0], 29.)
                 prelaunch_0 = nc.getvar(root, 'prelaunch_0')
-                self.assertEquals(prelaunch_0.shape, (final_size, 1, 1))
+                self.assertEquals(prelaunch_0.shape, (the_size, 1, 1))
                 self.assertAlmostEqual(prelaunch_0[0], 0.61180001, 6)
                 prelaunch_1 = nc.getvar(root, 'prelaunch_1')
-                self.assertEquals(prelaunch_1.shape, (final_size, 1, 1))
+                self.assertEquals(prelaunch_1.shape, (the_size, 1, 1))
                 self.assertAlmostEqual(prelaunch_1[0], 0.00116, 6)
                 postlaunch = nc.getvar(root, 'postlaunch')
-                self.assertEquals(postlaunch.shape, (final_size, 1, 1))
-        else:
-            last_image = (calibrator.get_datetime(self.initial_files[-1])
-                          if self.initial_files else datetime.utcnow())
-            next_image = last_image + timedelta(minutes=30)
-            if self.should_download(next_image):
-                time_from_last_image = datetime.utcnow() - last_image
-                self.assertLessEqual(time_from_last_image,
-                                     timedelta(minutes=30))
+                self.assertEquals(postlaunch.shape, (the_size, 1, 1))
 
 
 class TestInstrument(unittest.TestCase):
@@ -66,11 +61,11 @@ class TestInstrument(unittest.TestCase):
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
 
-    def assert_downloaded_files(self, should_download):
+    def assert_downloaded_files(self, should_download=None):
         return assert_downloaded(self, should_download)
 
     def test_download(self):
-        with self.assert_downloaded_files(lambda dt: True) as validation:
+        with self.assert_downloaded_files() as validation:
             files = goes.download('noaa.gvarim', 'noaaadmin', self.directory,
                               suscription_id='55253')
             validation.downloaded(files)
